fa8cd70dc7f9045678f271e513f3b71c3baaa76d
laz <you@example.com>
2018-12-25 16:23:03 +0200

    add iterations and cycles to parse

--- extensions/core/src/parse.lua
+++ extensions/core/src/parse.lua
@@ -2,7 +2,13 @@ local eval = require "eval"
 local util = require "util"
 
 
-local function transform (text, data, delimiter, nestedCycles, iterators, nestedConditions)
+local function multiplyString (str, n)
+	local s = ""
+	for i=1,n do s = s .. str end
+	return s
+end
+
+local function transform (text, data, delimiter, nestedCycles, nestedConditions)
 	local substrings = {} -- array with substrings
 
 	local iteratorIndex = 0
@@ -10,22 +16,66 @@ local function transform (text, data, delimiter, nestedCycles, iterators, nested
 		local startDelimiterIndex = text:indexOf(delimiter.open, iteratorIndex)
 		if startDelimiterIndex then
 			if delimiter.preserve then
-				substrings[#substrings + 1] = text:sub(iteratorIndex, startDelimiterIndex + delimiter.open:len() + 1)
+				substrings[#substrings + 1] = text:sub(iteratorIndex, startDelimiterIndex + delimiter.open:len() - 1)
 			else
 				substrings[#substrings + 1] = text:sub(iteratorIndex, startDelimiterIndex - 1)
 			end
+			
+			if text:charAt(startDelimiterIndex + delimiter.open:len() + nestedCycles - 1) == "#" then  -- cycle
+				local startExpressionIndex = startDelimiterIndex + delimiter.open:len() + nestedCycles + 1 -- one more because #
 
-			if text:charAt(startDelimiterIndex + delimiter.open:len() + nestedCycles) == "#" then  -- cycle
+				-- find array name
+				local arrayExpressionEndIndex = text:indexOf("\n", startExpressionIndex)
+				local arrayExpression = text:sub(startExpressionIndex, arrayExpressionEndIndex - 1)
+				local array = eval.code(arrayExpression, data, true)
+				
+				-- find element that will be repeated
+				local endDelimiter = delimiter.close .. multiplyString("#", nestedCycles)
+				local endDelimiterIndex = text:indexOf(endDelimiter, arrayExpressionEndIndex + 1)
+				while text:charAt(endDelimiterIndex + endDelimiter:len()) == "#" do
+					endDelimiterIndex = text:indexOf(endDelimiter, endDelimiterIndex + 1)
+				end
+				local element = text:sub(arrayExpressionEndIndex + 1, endDelimiterIndex - 1)
+				
+				-- iterate the array over the element
+				if array and #array > 0 then -- do not iterate over non-existing or empty arrays
+					local iterator = multiplyString("i", nestedCycles)
+					for i=1,#array do
+						data[iterator] = i
+						substrings[#substrings + 1] = transform(element, data, delimiter, nestedCycles + 1, nestedConditions)
+					end
+					data[iterator] = nil -- reset iterator
+				end
 
-			elseif text:charAt(startDelimiterIndex + delimiter.open:len() + nestedConditions) == "?" then  -- condition
+				iteratorIndex = endDelimiterIndex + delimiter.close:len() + nestedCycles + 1 -- one more because #
+			elseif text:charAt(startDelimiterIndex + delimiter.open:len() + nestedConditions - 1) == "?" then  -- condition
+				local startExpressionIndex = startDelimiterIndex + delimiter.open:len() + nestedConditions + 1 -- one more because ?
 
+				-- find condition name
+				local conditionExpressionEndIndex = text:indexOf("\n", startExpressionIndex)
+				local conditionExpression = text:sub(startExpressionIndex, conditionExpressionEndIndex - 1)
+				local condition = eval.code(conditionExpression, data, true)
+				
+				-- find element that will be conditioned
+				local endDelimiter = delimiter.close .. multiplyString("?", nestedConditions)
+				local endDelimiterIndex = text:indexOf(endDelimiter, conditionExpressionEndIndex + 1)
+				while text:charAt(endDelimiterIndex + endDelimiter:len()) == "?" do
+					endDelimiterIndex = text:indexOf(endDelimiter, endDelimiterIndex + 1)
+				end
+				local element = text:sub(conditionExpressionEndIndex + 1, endDelimiterIndex - 1)
+				
+				-- condition over the element
+				if condition then
+					substrings[#substrings + 1] = transform(element, data, delimiter, nestedCycles, nestedConditions + 1)
+				end
+				
+				iteratorIndex = endDelimiterIndex + delimiter.close:len() + nestedConditions + 1 -- one more because ?
 			else  -- replace
 				local startExpressionIndex = startDelimiterIndex + delimiter.open:len()
 				local endDelimiterIndex = text:indexOf(delimiter.close, startExpressionIndex)
 
 				local propertyName = text:sub(startExpressionIndex, endDelimiterIndex - 1)
-				local transformedPropertyName = transform(propertyName:trim(), iterators, {open = "[", close = "]", preserve = true}, 0, {}, 0)
-				substrings[#substrings + 1] = eval.code(transformedPropertyName, data, true)
+				substrings[#substrings + 1] = eval.code(propertyName, data, true)
 
 				if delimiter.preserve then
 					iteratorIndex = endDelimiterIndex
@@ -43,5 +93,5 @@ end
 
 
 return function (text, data)
-	return transform(text, data, {open = "{{", close = "}}", preserve = false}, 0, {}, 0)
+	return transform(text, data, {open = "{{", close = "}}", preserve = false}, 1, 1)
 end
