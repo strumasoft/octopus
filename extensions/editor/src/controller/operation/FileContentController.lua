d498337e93fc51bb9d8caf5aa685f5ee5c640fd6
strumasoft <13884213+strumasoft@users.noreply.github.com>
2023-03-01 02:08:46 +0200

    modify appearance on narrow screen

--- extensions/editor/src/controller/operation/FileContentController.lua
+++ extensions/editor/src/controller/operation/FileContentController.lua
@@ -7,8 +7,8 @@ local editor = require "Editor"
 
 local notPlainTextExtensions = [[
  flac mp3 ogg oga mogg raw voc vox wav wma wv webm 
- 3gp 3g2 flv f4v f4p f4a f4b ogv gif gifv avi mp4 m4p m4v mpg mp2 mpeg mpe mpv mts m2ts ts mov qt wmv yuv 
- jpg jpeg jpe jif jfif jfi png webp tiff tif psd raw arw cr2 nrw k25 bmp dib heif heic ind indd indt jp2 j2k jpf jpx jpm mj2 svg svgz ai eps 
+ 3gp 3g2 flv f4v f4p f4a f4b ogv gif gifv avi mp4 m4p m4v mpg mp2 mpeg mpe mpv mts m2ts mov qt wmv yuv 
+ jpg jpeg jpe jif jfif jfi png webp tiff tif psd raw arw cr2 nrw k25 bmp dib heif heic ind indd indt jp2 j2k jpf jpx jpm mj2 ico svgz ai eps 
  pdf doc docx dot odt ott xls xlsx ppt pptx 
  exe dll so o a ar cpio shar lbr iso mar sbx f xf lz lzma lzmo rz sfark sz q z xz 
  7z s7z ace afa alz apk arc ark cdx b1 cab dmg jar rar gz tgz tbz2 tar bz2 txz war zip zipx zz 
