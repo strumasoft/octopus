522a072390d3705e0e5bd0a83267c2cef8780fe5
laz <you@example.com>
2024-01-17 18:09:48 +0200

    make tabs spaces

--- bin/autowire.lua
+++ bin/autowire.lua
@@ -4,62 +4,62 @@ local oldRequire = require
 
 package.loaded.MODULES = {}
 local function configuration (moduleName)
-	local MODULES = package.loaded.MODULES
-	local octopusHostDir = ngx.var.octopusHostDir
+  local MODULES = package.loaded.MODULES
+  local octopusHostDir = ngx.var.octopusHostDir
 
-	if MODULES[octopusHostDir] then
-		return MODULES[octopusHostDir][moduleName]
-	else
-		local modulesConfig = dofile(octopusHostDir .. "/build/src/module.lua")
-		MODULES[octopusHostDir] = modulesConfig
-		return modulesConfig[moduleName]
-	end
+  if MODULES[octopusHostDir] then
+    return MODULES[octopusHostDir][moduleName]
+  else
+    local modulesConfig = dofile(octopusHostDir .. "/build/src/module.lua")
+    MODULES[octopusHostDir] = modulesConfig
+    return modulesConfig[moduleName]
+  end
 end
 
 
 local function newRequire (moduleName, newModule)
-	if not moduleName then return nil end
-	
-	local scripts = configuration(moduleName)
-	if scripts then
-		local key
-		if scripts[1]["extensionDir"]:find(ngx.var.octopusExtensionsDir, 1, true) then
-			key = "octopus." .. moduleName
-		elseif moduleName:find("global.", 1, true) then
-			key = moduleName
-		else
-			key = ngx.var.octopusHostDir .. ":" .. moduleName
-		end
-		
-		if newModule then
-			package.loaded[key] = newModule
-			return
-		end
-		
-		local cached = package.loaded[key]
-		if cached then return cached end
-		
-		local lastModule, tempModule
-		for i=#scripts, 2, -1 do -- first script holds metadata
-			local scriptModule = dofile(scripts[i])
+  if not moduleName then return nil end
+  
+  local scripts = configuration(moduleName)
+  if scripts then
+    local key
+    if scripts[1]["extensionDir"]:find(ngx.var.octopusExtensionsDir, 1, true) then
+      key = "octopus." .. moduleName
+    elseif moduleName:find("global.", 1, true) then
+      key = moduleName
+    else
+      key = ngx.var.octopusHostDir .. ":" .. moduleName
+    end
+    
+    if newModule then
+      package.loaded[key] = newModule
+      return
+    end
+    
+    local cached = package.loaded[key]
+    if cached then return cached end
+    
+    local lastModule, tempModule
+    for i=#scripts, 2, -1 do -- first script holds metadata
+      local scriptModule = dofile(scripts[i])
 
-			if i == #scripts then
-				lastModule = scriptModule
-				tempModule = lastModule
-			else
-				local nonLastModule = scriptModule
-				local mt = getmetatable(tempModule) or {}
-				mt.__index = nonLastModule
-				setmetatable(tempModule, mt)
-				tempModule = nonLastModule
-			end
-		end
-		
-		package.loaded[key] = lastModule
-		return lastModule
-	end
-	
-	return oldRequire(moduleName)
+      if i == #scripts then
+        lastModule = scriptModule
+        tempModule = lastModule
+      else
+        local nonLastModule = scriptModule
+        local mt = getmetatable(tempModule) or {}
+        mt.__index = nonLastModule
+        setmetatable(tempModule, mt)
+        tempModule = nonLastModule
+      end
+    end
+    
+    package.loaded[key] = lastModule
+    return lastModule
+  end
+  
+  return oldRequire(moduleName)
 end
 
 
--- bin/unix/build.lua
+++ bin/unix/build.lua
@@ -1,15 +1,15 @@
 -- load libraries --
 
 local loadExtension = function (extensionsDir, extensionName)
-	package.path = package.path .. ";" .. extensionsDir .. "/" .. extensionName .. "/src/?.lua"
+  package.path = package.path .. ";" .. extensionsDir .. "/" .. extensionName .. "/src/?.lua"
 end
 
 local loadPath = function (path)
-	package.path = package.path .. ";" .. path
+  package.path = package.path .. ";" .. path
 end
 
 local loadCPath = function (cpath)
-	package.cpath = package.cpath .. ";" .. cpath
+  package.cpath = package.cpath .. ";" .. cpath
 end
 
 local octopusExtensionsDir = "../../extensions"
@@ -24,7 +24,7 @@ loadExtension(octopusExtensionsDir, "orm")
 -- add project in the following relative form:
 -- key = {"../../../dir", "config.lua"}
 local hosts = {
-	extensions = {octopusExtensionsDir, "config.lua"},
+  extensions = {octopusExtensionsDir, "config.lua"},
 }
 
 local hostNames = {}
@@ -32,30 +32,30 @@ for k, v in pairs(hosts) do table.insert(hostNames, k) end
 
 local profiles = {}
 profiles.prod = {
-	profile = "prod", hosts = hostNames,
-	port = 8787, securePort = 38787, luaCodeCache = "on",
-	requireSecurity = false, sessionTimeout = 14400,
-	rdbms = "mysql", rdbms_host = "127.0.0.1", rdbms_port = "3306", rdbms_user = "demo", rdbms_password = "demo",
+  profile = "prod", hosts = hostNames,
+  port = 8787, securePort = 38787, luaCodeCache = "on",
+  requireSecurity = false, sessionTimeout = 14400,
+  rdbms = "mysql", rdbms_host = "127.0.0.1", rdbms_port = "3306", rdbms_user = "demo", rdbms_password = "demo",
 }
 profiles.test = {
-	profile = "test", hosts = hostNames,
-	port = 8787, securePort = 38787, luaCodeCache = "off",
-	requireSecurity = false, sessionTimeout = 3600,
-	rdbms = "mysql", rdbms_host = "127.0.0.1", rdbms_port = "3306", rdbms_user = "demo", rdbms_password = "demo",
+  profile = "test", hosts = hostNames,
+  port = 8787, securePort = 38787, luaCodeCache = "off",
+  requireSecurity = false, sessionTimeout = 3600,
+  rdbms = "mysql", rdbms_host = "127.0.0.1", rdbms_port = "3306", rdbms_user = "demo", rdbms_password = "demo",
 }
 
 local buildProfile = arg[1]
 if buildProfile then
-	print("[" .. buildProfile .. "]")
-	if buildProfile == "clear" then
-		config = profiles["test"]
-	elseif profiles[buildProfile] then 
-		config = profiles[buildProfile]
-	else
-		error(buildProfile .. " is not name of build profile")
-	end
+  print("[" .. buildProfile .. "]")
+  if buildProfile == "clear" then
+    config = profiles["test"]
+  elseif profiles[buildProfile] then 
+    config = profiles[buildProfile]
+  else
+    error(buildProfile .. " is not name of build profile")
+  end
 else
-	config = profiles["test"]
+  config = profiles["test"]
 end
 
 
@@ -64,29 +64,29 @@ end
 local nginxConf = [[
 http {
 
-	lua_package_path '../?.lua;lib/?.lua;;';
+  lua_package_path '../?.lua;lib/?.lua;;';
 
-	lua_package_cpath 'luajit/lib/?.so;;';
+  lua_package_cpath 'luajit/lib/?.so;;';
 
-	init_by_lua_block {
-		require "resty.core"
-		collectgarbage("collect")
-		require "cdefinitions"
-		require = require "autowire"
-	}
+  init_by_lua_block {
+    require "resty.core"
+    collectgarbage("collect")
+    require "cdefinitions"
+    require = require "autowire"
+  }
 
-	# mime types
-	include mime.types;
+  # mime types
+  include mime.types;
 
-	# applications
-	include nginx.config.*;
+  # applications
+  include nginx.config.*;
 
 } # end of http
 
 events { 
-	worker_connections  4096;
-	multi_accept        on;
-	use                 epoll;
+  worker_connections  4096;
+  multi_accept        on;
+  use                 epoll;
 } # end of events
 
 worker_processes auto;
@@ -98,15 +98,15 @@ worker_processes auto;
 local lfs = require "lfs"
 local dir = "."
 for entry in lfs.dir(dir) do
-	if entry ~= "." and entry ~= ".." then
-		local path
-		if dir ~= "/" then path = dir .. "/" .. entry else path = "/" .. entry end
+  if entry ~= "." and entry ~= ".." then
+    local path
+    if dir ~= "/" then path = dir .. "/" .. entry else path = "/" .. entry end
 
-		local attr = lfs.attributes(path)
-		if attr and attr.mode == "file" then
-			if path:find("nginx.config.", 1, true) then os.remove(path) end
-		end
-	end
+    local attr = lfs.attributes(path)
+    if attr and attr.mode == "file" then
+      if path:find("nginx.config.", 1, true) then os.remove(path) end
+    end
+  end
 end
 
 
@@ -122,25 +122,25 @@ file:close()
 local builder = require "builder"
 local eval = require "eval"
 for i=1,#config.hosts do
-	local hostDirName = config.hosts[i]
-	local extensionsDir = hosts[hostDirName][1]
-	local configFileName = hosts[hostDirName][2]
+  local hostDirName = config.hosts[i]
+  local extensionsDir = hosts[hostDirName][1]
+  local configFileName = hosts[hostDirName][2]
 
-	local function process (command)
-		print(command .. " server from " .. extensionsDir .. "/" .. configFileName)
-		
-		-- override site configFileName (config.lua) with the profile configurations
-		config.octopusExtensionsDir = lfs.currentdir() .. "/" .. octopusExtensionsDir
-		config.octopusHostDir = lfs.currentdir() .. "/" .. extensionsDir
-		local siteConfig = eval.file(extensionsDir .. "/" .. configFileName, config)
-		siteConfig.nginxConfFileName = "nginx.config." .. hostDirName
-		
-		builder[command](siteConfig)
-	end
-	
-	local command = "build"
-	if buildProfile == "clear" then command = "clear" end
-	
-	local status, err = pcall(process, command)
-	if not status then print(err) end
+  local function process (command)
+    print(command .. " server from " .. extensionsDir .. "/" .. configFileName)
+    
+    -- override site configFileName (config.lua) with the profile configurations
+    config.octopusExtensionsDir = lfs.currentdir() .. "/" .. octopusExtensionsDir
+    config.octopusHostDir = lfs.currentdir() .. "/" .. extensionsDir
+    local siteConfig = eval.file(extensionsDir .. "/" .. configFileName, config)
+    siteConfig.nginxConfFileName = "nginx.config." .. hostDirName
+    
+    builder[command](siteConfig)
+  end
+  
+  local command = "build"
+  if buildProfile == "clear" then command = "clear" end
+  
+  local status, err = pcall(process, command)
+  if not status then print(err) end
 end
